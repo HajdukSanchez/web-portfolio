6bd0f37b7c0e5e49351762bce224a57ecc06738e
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-09-02 17:35:16 -0500

    :lipstick: [ADD] Styles for Mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,37 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 // * Styles
-import { Navigator, List, ListItem } from '../../../styles/components/Header'
+import {
+  HeaderMenu,
+  Navigator,
+  NavigatorLinks,
+  List,
+  ListItem,
+  MenuHamb,
+} from '../../../styles/components/Header'
+// * Icons
+import { VscGithubInverted, VscColorMode, VscCode } from 'react-icons/vsc'
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [showMenu, setShowMenu] = useState(false)
+
+  const handleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+    if (isMenuOpen) {
+      setShowMenu(true)
+    } else {
+      setShowMenu(false)
+    }
+  }
+
   return (
     <header>
-      <Navigator>
-        <List>
-          <ListItem>
-            <Link to='/'>logo</Link>
-          </ListItem>
-          <ListItem>
-            <Link to=''>about me</Link>
-          </ListItem>
-          <ListItem>
-            <Link to=''>blog</Link>
-          </ListItem>
-          <ListItem>
-            <Link to=''>works</Link>
-          </ListItem>
-          <ListItem>
-            <Link to=''>studies</Link>
-          </ListItem>
-          <ListItem>
-            <Link to=''>github</Link>
-          </ListItem>
-        </List>
-      </Navigator>
+      <HeaderMenu>
+        <div>
+          <Link to='/'>logo</Link>
+        </div>
+        <NavigatorLinks isHidden isOpen={showMenu}>
+          <List>
+            <ListItem>
+              <Link to='/about-me'>about me</Link>
+            </ListItem>
+            <ListItem>
+              <Link to='/blog'>blog</Link>
+            </ListItem>
+            <ListItem>
+              <Link to='/works'>works</Link>
+            </ListItem>
+            <ListItem>
+              <Link to='/certificates'>certificates</Link>
+            </ListItem>
+          </List>
+        </NavigatorLinks>
+        <Navigator>
+          <List>
+            <ListItem isButton isHidden>
+              <Link to=''>cv</Link>
+            </ListItem>
+            <ListItem>
+              <VscColorMode />
+            </ListItem>
+            <ListItem>
+              <a
+                href='https://github.com/HajdukSanchez'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <VscGithubInverted />
+              </a>
+            </ListItem>
+          </List>
+        </Navigator>
+        <MenuHamb>
+          <VscCode style={{ fill: 'white' }} onClick={() => handleMenu()} />
+        </MenuHamb>
+      </HeaderMenu>
     </header>
   )
 }
 
 export default Header
 
-// TODO: ADD GITHUB AND LOCAL LOGOS
+// TODO: ADD LOCAL LOGO
+// TODO: Add dark mode button functionality
--- styles/components/Header.js
+++ styles/components/Header.js
@@ -1,18 +1,57 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Navigator = styled.nav`
+export const HeaderMenu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   margin: 0 auto;
   padding: 1.5rem;
-  width: 50%;
+  width: 100%;
+`
+
+export const Navigator = styled.nav`
+  display: flex;
+`
+
+export const NavigatorLinks = styled.nav`
+  display: flex;
+  ${(props) =>
+    props.isHidden &&
+    css`
+      display: none;
+    `}
 `
 
 export const List = styled.ul`
   display: flex;
   justify-content: center;
+  align-items: center;
   font-size: 1.6rem;
 `
 
 export const ListItem = styled.li`
-  margin: 0 4rem;
-  padding: 0 1rem;
+  margin: 0 2rem;
+  padding: 0 1.5rem;
+  text-transform: capitalize;
+  cursor: pointer;
+  ${(props) =>
+    props.isButton &&
+    css`
+      padding: 0.5rem 3rem;
+      border: 1px solid black;
+      border-radius: 6px;
+    `}
+  ${(props) =>
+    props.isHidden &&
+    css`
+      display: none;
+    `}
+`
+
+export const MenuHamb = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+  cursor: pointer;
 `
